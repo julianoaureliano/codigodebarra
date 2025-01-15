2f22a79fcca542711c0ea2c7a6633e8df9e2eea1
JulianoAureliano <juliano.aureliano2013@gmail.com>
2025-01-15 17:53:06 -0300

    Leitor completo

--- leitorcodigodebarra.c
+++ leitorcodigodebarra.c
@@ -37,6 +37,72 @@ void tirar_espacamento_lateral(CodigoBarraDados *dados, ConfiguracaoCodigoBarra 
         dados->index_digitos++;
     }
 }
+// remover o 101 inicial e final do vetor
+void remover_marcadores(CodigoBarraDados *dados)
+{
+    int inicial = 3;
+    int final = 3;
+    // desloca os elementos pra esquerda, ignorando os primeiros
+    for (int i = 0; i < dados->index_digitos - inicial - final; i++)
+    {
+        dados->todos_os_digitos[i] = dados->todos_os_digitos[i + inicial];
+    }
+    dados->index_digitos -= (inicial + final);
+}
+// remover o 01010 no meio do vetor
+void remover_marcador_central(CodigoBarraDados *dados)
+{
+    int inicioRemocao = 28; // sao 28 digitos antes do caracter central pois 7*4=28
+    int quantidade = 5;     // tamanho do 01010
+    // desloca os elementos do vetor para preencher a lacuna
+    for (int i = inicioRemocao; i < dados->index_digitos - quantidade; i++)
+    {
+        dados->todos_os_digitos[i] = dados->todos_os_digitos[i + quantidade];
+    }
+    dados->index_digitos -= (quantidade);
+}
+
+void agrupar_em_strings(CodigoBarraDados *dados, char resultadoEmString[][8], int *tamanhoResultado)
+{
+    int grupos = dados->index_digitos / 7; // grupos de 7
+    *tamanhoResultado = grupos;
+
+    for (int i = 0; i < grupos; i++)
+    {
+        // Inicializa a string para o grupo atual
+        for (int j = 0; j < 7; j++)
+        {
+            resultadoEmString[i][j] = '0' + dados->todos_os_digitos[i * 7 + j]; // Converte 0 ou 1 para caractere
+        }
+        resultadoEmString[i][7] = '\0'; // Termina a string com '\0'
+    }
+}
+// vou comparar os grupos de string que fiz com o lcode e rcode, e guardo a posição que na verdade ja é o numero
+void transformar_verificador(CodigoBarraTabela *tabela, ConfiguracaoCodigoBarra *config, char resultadoEmString[][8], int tamanhoResultado)
+{
+    for (int i = 0; i < tamanhoResultado; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            if (i < 5)
+            {
+                if (strcmp(resultadoEmString[i], tabela->Lcode[j]) == 0)
+                {
+                    config->digitos[i] = j;
+                    break;
+                }
+            }
+            else
+            {
+                if (strcmp(resultadoEmString[i], tabela->Rcode[j]) == 0)
+                {
+                    config->digitos[i] = j;
+                    break;
+                }
+            }
+        }
+    }
+}
 
 int main()
 {
@@ -44,6 +110,9 @@ int main()
     CodigoBarraDados dados = {.index_digitos = 0, .index_pixels_tratados = 0};
     ConfiguracaoCodigoBarra config = {.espacamento_lateral = 0, .altura_dos_pixels = 0};
     int quantidade_de_pixels = 0;
+    CodigoBarraTabela tabela = {
+        .Lcode = {"0001101", "0011001", "0010011", "0111101", "0100011", "0110001", "0101111", "0111011", "0110111", "0001011"},
+        .Rcode = {"1110010", "1100110", "1101100", "1000010", "1011100", "1001110", "1010000", "1000100", "1001000", "1110100"}};
 
     FILE *file = fopen("codigo_de_barras.pbm", "r");
     if (!file)
@@ -87,10 +156,34 @@ int main()
 
     printf("\n");
     tirar_espacamento_lateral(&dados, &config, &quantidade_de_pixels);
+    remover_marcadores(&dados);
+    remover_marcador_central(&dados);
+    char resultadoEmString[dados.index_digitos / 7][8];
+    int tamanhoResultado;
+    agrupar_em_strings(&dados, resultadoEmString, &tamanhoResultado);
+    printf("\n");
+
+    for (int i = 0; i < dados.index_digitos; i++)
+    {
+        printf("%d", dados.todos_os_digitos[i]);
+    }
 
     printf("\n");
     printf("Espacamento lateral do codigo eh %d e a resolucao dos pixels eh %d tamanho da linha configurada %d", config.espacamento_lateral, config.altura_dos_pixels, dados.index_digitos);
-    // Liberando memória e fechando o arquivo
+    printf("\n");
+    // Imprime o resultado
+    for (int i = 0; i < tamanhoResultado; i++)
+    {
+        printf("%s\n", resultadoEmString[i]);
+    }
+    transformar_verificador(&tabela, &config, resultadoEmString, tamanhoResultado);
+
+    printf("Identificador do codigo de barras: ");
+    for (int i = 0; i < 8; i++)
+    {
+        printf("%d", config.digitos[i]);
+    }
+    // liberando memória e fechando o arquivo
     free(pixels);
     fclose(file);
 
